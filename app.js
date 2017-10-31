362b2e07e184b0f10b6ed53b9089849a7bf25682
fazilham <fazilvellore@gmail.com>
2017-10-31 17:05:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,12 +6,7 @@ var restify = require('restify');
 var Promise = require('bluebird');
 var url = require('url');
 var Swagger = require('swagger-client');
-
-
-
 var schedule = require('node-schedule');
-
-
 // Swagger client for Bot Connector API
 var connectorApiClient = new Swagger({
     url: 'https://raw.githubusercontent.com/Microsoft/BotBuilder/master/CSharp/Library/Microsoft.Bot.Connector.Shared/Swagger/ConnectorAPI.json',
@@ -22,7 +17,6 @@ var connectorApiClient = new Swagger({
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
     console.log('%s listening to %s', server.name, server.url);
-    console.log('Time:'+ new Date())
 });
 
 // Create chat bot
@@ -35,23 +29,50 @@ var connector = new builder.ChatConnector({
 server.post('/api/messages', connector.listen());
 
 // Bot setup
-var bot = new builder.UniversalBot(connector, function (session) {
- 
-});
+var msgAddress= [];
 
+var bot = new builder.UniversalBot(connector, function (session) {
+});
 
 bot.on('conversationUpdate', function (message) {
-    
-    console.log('conversationUpdate event triggered');
-    
-    var j = schedule.scheduleJob({hour: 10, minute: 20, dayOfWeek: 1}, function(){
-    console.log('Test conversatiion Schedule');
-   bot.send(new builder.Message()
-                .address(message.address)
-                .text('Hi All, Good morning /nPlease update the sheet for food preference for team lunch today. /n https://zetacorporate-my.sharepoint.com/personal/vsankarayogi_zetaglobal_com/_layouts/15/WopiFrame2.aspx?sourcedoc=%7B7CD23D7E-188D-4A3A-A586-AC4D417E563C%7D&file=Team%20Lunch.xlsx&action=default'));
+
+    var address = message.address;
+
+    msgAddress.push(address);
+    console.log(1)
+    console.log(message.address)
+    console.log(2)
+   /* bot.send(new builder.Message()
+        .address(message.address)
+        .text('Welcome'));*/
+
+
 });
 
-    
+var j = schedule.scheduleJob({hour: 11, minute: 40, dayOfWeek: 2}, function(){
+   
+
+
+    console.log('running every minute 1, 2, 4 and 5');
+
+    console.log('1')
+    console.log(msgAddress);
+    console.log(2)
+    var newAddresses = msgAddress.splice(0);
+    console.log(3)
+    console.log(newAddresses)
+
+    msgAddress.forEach(function (address) {
+        console.log('Crone 1')
+        console.log(address.address)
+        console.log('Crone 1')
+        bot.send(new builder.Message()
+            .address(address.address)
+            .text('Welcome'));
+
+    })
+
+
 });
 
 
