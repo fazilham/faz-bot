d094ff35297f9a9644ae565bd5609f3dc414c7bc
fazilham <fazilvellore@gmail.com>
2017-11-19 13:23:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,10 +20,8 @@ var connector = new builder.ChatConnector({
 // Listen for messages
 server.post('/api/messages', connector.listen());
 
-function sendProactiveMessage(address) {
-    var msg = new builder.Message().address(address);
-    msg.text('Hello, this is a notification');
-    msg.textLocale('en-US');
+function sendProactiveMessage(address, txt) {
+    var msg = new builder.Message().address(address).text(txt);
     bot.send(msg);
 }
 
@@ -33,38 +31,44 @@ var bot = new builder.UniversalBot(connector, function (session) {
 });
 
 
-bot.on('conversationUpdate', function (message) {
-   if (message.membersAdded && message.membersAdded.length > 0) {
-        var botAddId = message.address.bot.id;
 
+
+bot.on('conversationUpdate', function (message) {
+
+    if (message.membersAdded && message.membersAdded.length > 0) {
+        var botAddId = message.address.bot.id;
         for (var i = 0; i < message.membersAdded.length; i++) {
             if (message.membersAdded[i].id === botAddId) {
-                console.log('Bot Fired');
-                
-                var schedule = new cron.CronJob({
-        cronTime: '00 25 21 * * 6',
-        onTick: function() {
-            console.log('job 1 ticked');
-            sendProactiveMessage(message.address)
-        },
-        start: true,
-        timeZone: 'Asia/Kolkata'
-    });
-                
-
-                // Say goodbye
-                /*var reply = new builder.Message()
-                    .address(message.address)
-                    .text("Goodbye");
-                bot.send(reply);
-                break;*/
+                console.log('Bot Fired')
+                var schedule1 = new cron.CronJob({
+                    cronTime: '0 30 13 * * *',
+                    onTick: function () {
+                        console.log('Schedule 1 fired at 9am');
+                        sendProactiveMessage(message.address, 'Hi All, Good morning!  \n  Please update the sheet for food preference for team lunch today.  \n  https://zetacorporate-my.sharepoint.com/personal/vsankarayogi_zetaglobal_com/_layouts/15/WopiFrame2.aspx?sourcedoc=%7B7CD23D7E-188D-4A3A-A586-AC4D417E563C%7D&file=Team%20Lunch.xlsx&action=default')
+                    },
+                    start: true,
+                    timeZone: 'Asia/Kolkata'
+                })
+                var schedule2 = new cron.CronJob({
+                    cronTime: '00 35 13 * * 7',
+                    onTick: function () {
+                        console.log('Reminder Schedule at 10:30');
+                        sendProactiveMessage(message.address, 'This is a reminder that we are going to place an order at 11:30 based on the sheet updated')
+                    },
+                    start: true,
+                    timeZone: 'Asia/Kolkata'
+                })
+                var schedule3 = new cron.CronJob({
+                    cronTime: '00 40 13 * * 7',
+                    onTick: function () {
+                        console.log('Reminder Schedule at 11:15 ');
+                        sendProactiveMessage(message.address, 'This is a reminder that we are going to place an order at 11:30 based on the sheet updated')
+                    },
+                    start: true,
+                    timeZone: 'Asia/Kolkata'
+                })
             }
         }
 
     }
-
-    
-
-
-    /*sendProactiveMessage(message.address)*/
 });
